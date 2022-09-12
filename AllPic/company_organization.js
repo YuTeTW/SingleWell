07a859c82a1f07ec50572d2e1808b3127003787a
Dave <judhaha@gmail.com>
2022-09-12 17:26:42 +0800

    Initial commit

---
+++ AllPic/company_organization.js
@@ -0,0 +1,23 @@
+
+let pagedata = {
+  type: 1,
+  language: 0,
+  style: [
+    {
+      key: 'margin-bottom', 
+      value: '20px'
+    }
+  ],
+  data: [
+    {
+      block_title: '公司組織',
+      type: 3,
+      style: 1,
+      title: '',
+      content: '',
+      value: '../../assets/images/organization.png'
+    },
+  ],
+}
+
+export default pagedata
